d7f1b380e653638cc5cae8f854c4041b54011080
Atharva Chauthaiwale <achauthaiwale@slack-corp.com>
2020-10-25 13:19:56 +0530

    walking skeleton css

--- src/main/java/com/manning/cakefactory/controllers/CakeFactoryController.java
+++ src/main/java/com/manning/cakefactory/controllers/CakeFactoryController.java
@@ -1,4 +1,4 @@
-package com.manning.controllers;
+package com.manning.cakefactory.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/test/java/com/manning/cakefactory/controllers/CakeFactoryControllerTest.java
@@ -0,0 +1,29 @@
+package com.manning.cakefactory.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.hamcrest.Matchers.containsString;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CakeFactoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"))
+                .andExpect(content().string(containsString("Cake Factory")));
+    }
+}
